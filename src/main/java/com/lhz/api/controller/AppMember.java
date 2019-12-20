6a0ad0c1b9f4d30c2eec27e152efe4c1fb987064
lhz <lvhaozhou@zxint.com>
2019-12-20 12:16:25 +0800

    第一次提交

---
+++ src/main/java/com/lhz/api/controller/AppMember.java
@@ -0,0 +1,21 @@
+package com.lhz.api.controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: lhz
+ * @Date: 2019/12/7 0007 15:52
+ * @Description: member启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AppMember {
+    //@EnableDiscoveryClient 要想将一个微服务注册到Eureka Server（或其他服务发现组件，例如Zookeeper、Consul等
+    public static void main(String[] args) {
+        SpringApplication.run(AppMember.class, args);
+    }
+}
